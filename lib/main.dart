5dc53fed6245ebb66c4f7e705daa1500f5112922
daniel <armando.aleman.alp@cbtis258.edu.mx>
2025-10-20 08:37:48 -0600

    inicio de sesion activa

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'login.dart';
+import 'dashboard.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,10 +17,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: const LoginPage(),
+      title: 'Firebase Auth Demo',
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: const AuthWrapper(), // 👈 esta clase decide adónde ir
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(), // 👈 detecta usuario activo
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+        if (snapshot.hasData) {
+          return const DashboardPage(); // 👈 si hay sesión activa
+        }
+        return const LoginPage(); // 👈 si no hay sesión
+      },
     );
   }
 }
